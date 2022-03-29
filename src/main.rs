1020bcbd68c331e194f0c10aa3b0cfe8ce4fecf2
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2022-03-29 14:22:41 -0700

    Implement a REPL and support for running files

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,49 @@
-use std::process;
+use std::{env, fs, io, process};
 
 use rlox::chunk::{Chunk, OpCode};
 use rlox::debug::disassemble_chunk;
-use rlox::vm::VM;
+use rlox::vm::{InterpretResult, VM};
+
+fn read_file(filename: &str) -> String {
+    fs::read_to_string(filename).unwrap()
+}
+
+fn repl(vm: &mut VM) {
+    let mut line = String::new();
+    loop {
+        line.clear();
+        print!("> ");
+        if let Err(_) = io::stdin().read_line(&mut line) {
+            println!();
+            break;
+        }
+
+        vm.interpret(&line);
+    }
+}
+
+fn run_file(vm: &mut VM, filename: &str) {
+    let source = read_file(filename);
+    match vm.interpret(&source) {
+        InterpretResult::Ok => {}
+        InterpretResult::CompileError => process::exit(65),
+        InterpretResult::RuntimeError => process::exit(70),
+    }
+}
 
 fn main() {
     let mut vm = VM::new();
 
-    let mut chunk = Chunk::new();
-
-    let constant = chunk.add_constant(1.2);
-    chunk.write(OpCode::Constant.into(), 123);
-    chunk.write(constant as u8, 123);
-
-    let constant = chunk.add_constant(3.4);
-    chunk.write(OpCode::Constant.into(), 123);
-    chunk.write(constant as u8, 123);
-
-    chunk.write(OpCode::Add.into(), 123);
-
-    let constant = chunk.add_constant(5.6);
-    chunk.write(OpCode::Constant.into(), 123);
-    chunk.write(constant as u8, 123);
-
-    chunk.write(OpCode::Divide.into(), 123);
-
-    chunk.write(OpCode::Negate.into(), 123);
-    chunk.write(OpCode::Return.into(), 123);
-    disassemble_chunk(&chunk, "test chunk");
-
-    vm.interpret(&chunk);
+    let args: Vec<_> = env::args().collect();
+    if args.len() == 1 {
+        repl(&mut vm);
+    } else if args.len() == 2 {
+        run_file(&mut vm, &args[1]);
+    } else {
+        eprintln!("Usage: rlox [path]");
+        process::exit(64);
+    }
 
     drop(vm);
-    drop(chunk);
     process::exit(0);
 }
